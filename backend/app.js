bcb0e2db2ba4c1337aeed5a796fc981d25081591
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-03-23 13:47:29 +0300

    fix: изменить порт для бэкенда

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // Слушаем 3000 порт
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
