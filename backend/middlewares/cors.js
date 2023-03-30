8e1b553cfc7f35ac91a28f95e1e3ffdd632eb1d1
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-03-30 12:36:34 +0300

    fix:изменить адреса разрешенных доменов

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,8 @@
 const allowedCors = [
-  'https://projectmesto.savinova.nomoredomains.work',
-  'http://projectmesto.savinova.nomoredomains.work',
-  'https://api.projectmesto.savinova.nomoredomains.work',
-  'http://api.projectmesto.savinova.nomoredomains.work',
+  'https://projectmesto.savinova.nomoredomains.work/',
+  'http://projectmesto.savinova.nomoredomains.work/',
+  'https://api.projectmesto.savinova.nomoredomains.work/',
+  'http://api.projectmesto.savinova.nomoredomains.work/',
   'http://localhost:3000'];
 
 const corsOptions = {
