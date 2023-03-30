281d67df0a129666a57e8dcd11554d6f78dd76d6
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-03-30 17:20:13 +0300

    feat: добавить краш тест сервера

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,12 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routes);
 
 app.use(errorLogger);
