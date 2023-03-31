74a91220dd83b8eb3fbb92b868b317f1ec473844
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-03-31 09:48:42 +0300

    feat: добавить адреса доменов апи

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,10 @@
 const allowedCors = [
   'http://projectmesto.savinova.nomoredomains.work',
   'https://projectmesto.savinova.nomoredomains.work',
-  'http://localhost:3001'];
+  'http://localhost:3001',
+  'http://api.projectmesto.savinova.nomoredomains.work',
+  'https://api.projectmesto.savinova.nomoredomains.work',
+  'http://localhost:3000'];
 
 const corsOptions = {
   origin: allowedCors,
