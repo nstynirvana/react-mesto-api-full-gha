e952ad8ad57fd1eb5858681da64092b135f60684
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-03-31 11:51:32 +0300

    fix: изменить параметры cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,6 +10,8 @@ const corsOptions = {
   origin: allowedCors,
   optionsSuccessStatus: 200,
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+  preflightContinue: false,
 };
 
 module.exports = corsOptions;
