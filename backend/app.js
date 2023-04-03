9b737c7c9043455d82ea79a6e7514f978e432efe
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-04-03 14:52:11 +0300

    fix:изменить подключение к монго

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,8 @@ const app = express();
 
 app.use(cors(corsOptions));
 
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
+
 mongoose.connect(
   'mongodb://127.0.0.1:27017/mestodb',
   {
